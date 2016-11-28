f4694e67704fb142c71d1b7d4f97decc0efa5d0a
Bibby <bibbynet@gmail.com>
2016-11-28 17:56:44 +0800

    try to fix compiling errors again

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="globals/mongodb/index.d.ts" />
 /// <reference path="globals/mongoose-promise/index.d.ts" />
 /// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/mpromise/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 
 export {RepositoryBase} from "./../src/code/repositoryBase";
